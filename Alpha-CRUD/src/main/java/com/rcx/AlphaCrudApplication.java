54cd8b49f905d2613e29d9b6cbef420b797b1b84
atirpathi <atirpathi@LMIPl-157>
2022-11-24 04:44:10 +0530

    Add actuators

--- Alpha-CRUD/src/main/java/com/rcx/AlphaCrudApplication.java
+++ Alpha-CRUD/src/main/java/com/rcx/AlphaCrudApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+
 public class AlphaCrudApplication {
 	
 	private static Logger logger=LoggerFactory.getLogger(AlphaCrudApplication.class);
--- BetaConsumer-SpringBootMVC/src/main/java/com/beta/BetaConsumerSpringBootMvcApplication.java
+++ BetaConsumer-SpringBootMVC/src/main/java/com/beta/BetaConsumerSpringBootMvcApplication.java
@@ -2,12 +2,15 @@ package com.beta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@LoadBalancerClients
 public class BetaConsumerSpringBootMvcApplication {
 
 	public static void main(String[] args) {
